db9288ebbc640150cdd2429d7f4f61b362585d48
Dairon Amador <daironamador@outlook.com>
2024-08-01 18:13:51 -0400

    Album art actualizado v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const RadioPlayer = () => {
   const [playing, setPlaying] = useState(true);
   const [currentSong, setCurrentSong] = useState({
     title: 'Loading...',
-    image: 'https://losaradio.com/appadmin/losaimg.jpeg',
+    image: '../public/losaimg.jpeg', // This will always be used
   });
 
   useEffect(() => {
@@ -19,15 +19,9 @@ const RadioPlayer = () => {
         const data = await response.json();
         const trackData = data.data[0].track;
         
-        // Determine which image to display
-        const imageUrl =
-          trackData.album === null
-            ? 'https://losaradio.com/appadmin/losaimg.jpeg' // Default icon image
-            : trackData.imageurl;
-
         setCurrentSong({
           title: `${trackData.artist} - ${trackData.title}`,
-          image: imageUrl,
+          image: '../public/losaimg.jpeg', // This will always be used
         });
       } catch (error) {
         console.error('Error fetching current song:', error);
@@ -41,30 +35,33 @@ const RadioPlayer = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-white homebg">
-      <img
-        src={currentSong.image}
-        alt="Current song"
-        className="w-48 h-48 rounded-full mb-4"
-      />
-      <h1 className="text-2xl mb-2">LOSA Radio - La más completa</h1>
-      <h2 className="text-xl mb-4">{currentSong.title}</h2>
-      <ReactHowler
-        src="http://65.108.98.93:7528/stream"
-        playing={playing}
-        html5={true}
-        onLoad={() => console.log('Stream loaded successfully')}
-        onPlay={() => console.log('Stream playing')}
-        onPause={() => console.log('Stream paused')}
-        onEnd={() => console.log('Stream ended')}
-        onError={(error) => console.error('Stream error:', error)}
-      />
-      <button
-        className="bg-blue-500 px-4 py-2 rounded flex items-center"
-        onClick={() => setPlaying(!playing)}
-      >
-        {playing ? <FaPause /> : <FaPlay />}
-        <span className="ml-2">{playing ? 'Pause' : 'Play'}</span>
-      </button>
+      <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(/bglosa.png)' }}></div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full">
+        <img
+          src={currentSong.image}
+          alt="Current song"
+          className="w-48 h-48 rounded-full mb-4"
+        />
+        <h1 className="text-2xl mb-2">LOSA Radio - La más completa</h1>
+        <h2 className="text-xl mb-4">{currentSong.title}</h2>
+        <ReactHowler
+          src="http://65.108.98.93:7528/stream"
+          playing={playing}
+          html5={true}
+          onLoad={() => console.log('Stream loaded successfully')}
+          onPlay={() => console.log('Stream playing')}
+          onPause={() => console.log('Stream paused')}
+          onEnd={() => console.log('Stream ended')}
+          onError={(error) => console.error('Stream error:', error)}
+        />
+        <button
+          className="bg-blue-500 px-4 py-2 rounded flex items-center"
+          onClick={() => setPlaying(!playing)}
+        >
+          {playing ? <FaPause /> : <FaPlay />}
+          <span className="ml-2">{playing ? 'Pause' : 'Play'}</span>
+        </button>
+      </div>
     </div>
   );
 };
