266c6134193daefa0fc3f9fcaaab086fc00e4897
Dairon Amador <daironamador016@gmail.com>
2026-06-17 23:18:17 -0400

    fix: return null in service worker map callback to satisfy ESLint array-callback-return

--- src/service-worker.js
+++ src/service-worker.js
@@ -79,6 +79,7 @@ self.addEventListener('activate', (event) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
+          return null;
         })
       );
     })
